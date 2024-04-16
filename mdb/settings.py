97f04b3d4abdc105ede874fc310bf8e9a9c11691
jairomelo <jairomelo@gmail.com>
2024-04-16 10:58:23 +0100

    implementing ManifestStaticFilesStorage

--- mdb/settings.py
+++ mdb/settings.py
@@ -151,6 +151,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
