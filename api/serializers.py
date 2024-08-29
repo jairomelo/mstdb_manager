17cbb26bb5bc087ab418fda3facfdde02fc7dc0b
jairomelo <jairoantoniomelo@gmail.com>
2024-08-28 22:50:29 -0600

    full search implemented

--- api/serializers.py
+++ api/serializers.py
@@ -1,34 +1,95 @@
 from rest_framework import serializers
-from dbgestor.models import Archivo, Documento, PersonaEsclavizada, PersonaNoEsclavizada, Corporacion
-
+from dbgestor.models import (Archivo, Documento, PersonaEsclavizada, PersonaNoEsclavizada, Corporacion,
+                             PersonaLugarRel)
+from django.db.models import Manager
 
 class ArchivoSerializer(serializers.ModelSerializer):
-    
+    nombre_abreviado = serializers.CharField(read_only=True)
+    archivo_idno = serializers.CharField(read_only=True)
+
     class Meta:
         model = Archivo
-        fields = '__all__'
+        fields = ['archivo_id', 'nombre', 'nombre_abreviado', 'archivo_idno', 'created_at', 'updated_at']
 
 class DocumentoSerializer(serializers.ModelSerializer):
-    
+    archivo = ArchivoSerializer(read_only=True)
+    tipo_documento = serializers.StringRelatedField()
+    lugar_de_produccion = serializers.StringRelatedField()
+
     class Meta:
         model = Documento
-        fields = '__all__'
-        
-    
+        fields = ['documento_id', 'documento_idno', 'archivo', 'fondo', 'subfondo', 'serie', 'subserie',
+                  'tipo_udc', 'unidad_documental_compuesta', 'tipo_documento', 'sigla_documento',
+                  'titulo', 'descripcion', 'deteriorado', 'fecha_inicial', 'fecha_final',
+                  'lugar_de_produccion', 'folio_inicial', 'folio_final', 'notas', 'created_at', 'updated_at']
+
 class PersonaEsclavizadaSerializer(serializers.ModelSerializer):
-    
+    hispanizacion = serializers.SerializerMethodField()
+    etnonimos = serializers.SerializerMethodField()
+    procedencia = serializers.SerializerMethodField()
+
     class Meta:
         model = PersonaEsclavizada
-        fields = '__all__'
-        
+        fields = ['persona_id', 'persona_idno', 'nombre_normalizado', 'nombres', 'apellidos',
+                  'sexo', 'edad', 'unidad_temporal_edad', 'altura', 'cabello', 'ojos',
+                  'hispanizacion', 'etnonimos', 'procedencia', 'procedencia_adicional',
+                  'marcas_corporales', 'conducta', 'salud', 'created_at', 'updated_at']
+
+    def get_hispanizacion(self, obj):
+        return self.get_attribute_or_none(obj, 'hispanizacion')
+
+    def get_etnonimos(self, obj):
+        return self.get_attribute_or_none(obj, 'etnonimos')
+
+    def get_procedencia(self, obj):
+        return self.get_attribute_or_none(obj, 'procedencia')
+
+    def get_attribute_or_none(self, obj, attr):
+        if hasattr(obj, attr):
+            value = getattr(obj, attr)
+            if isinstance(value, Manager):
+                return [str(item) for item in value.all()]
+            return str(value) if value else None
+        return None
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        return {k: v for k, v in representation.items() if v is not None}
+
 class PersonaNoEsclavizadaSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = PersonaNoEsclavizada
-        fields = '__all__'
+        fields = ['persona_id', 'persona_idno', 'nombre_normalizado', 'nombres', 'apellidos',
+                  'sexo', 'entidad_asociada', 'honorifico', 'created_at', 'updated_at']
 
 class CorporacionSerializer(serializers.ModelSerializer):
-    
+    personas_asociadas = PersonaNoEsclavizadaSerializer(many=True, read_only=True)
+
     class Meta:
         model = Corporacion
-        fields = '__all__'
+        fields = ['corporacion_id', 'nombre_institucion', 'tipo_institucion', 'personas_asociadas',
+                  'created_at', 'updated_at']
+
+class PersonaLugarRelSerializer(serializers.ModelSerializer):
+    personas = PersonaEsclavizadaSerializer(many=True, read_only=True)
+    lugar = serializers.StringRelatedField()
+    situacion_lugar = serializers.StringRelatedField()
+    documento = DocumentoSerializer(read_only=True)
+
+    class Meta:
+        model = PersonaLugarRel
+        fields = ['persona_x_lugares', 'documento', 'personas', 'lugar', 'situacion_lugar',
+                  'ordinal', 'fecha_inicial_lugar', 'fecha_inicial_lugar_raw',
+                  'fecha_inicial_lugar_factual', 'fecha_final_lugar', 'fecha_final_lugar_raw',
+                  'fecha_final_lugar_factual', 'notas', 'created_at', 'updated_at']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['personas'] = [
+            {
+                'persona_id': persona.persona_id,
+                'persona_idno': persona.persona_idno,
+                'nombre_normalizado': persona.nombre_normalizado
+            } for persona in instance.personas.all()
+        ]
+        return representation
--- api/views.py
+++ api/views.py
@@ -5,9 +5,10 @@ from rest_framework import generics, viewsets, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from dbgestor.models import (Archivo, Documento, PersonaEsclavizada, PersonaNoEsclavizada, Corporacion)
+from dbgestor.models import (Archivo, Documento, PersonaEsclavizada, PersonaNoEsclavizada, Corporacion,
+                             PersonaLugarRel)
 from .serializers import (ArchivoSerializer, DocumentoSerializer, PersonaEsclavizadaSerializer, 
-                          PersonaNoEsclavizadaSerializer, CorporacionSerializer)
+                          PersonaNoEsclavizadaSerializer, CorporacionSerializer, PersonaLugarRelSerializer)
 
 # Create your views here.
 
@@ -29,6 +30,7 @@ class PersonaEsclavizadaViewSet(viewsets.ModelViewSet):
 class SearchAPIView(APIView):
     """
     API view to handle search queries across multiple models with custom pagination.
+    Supports exact phrase matching using quotes.
     """
     
     def get(self, request, *args, **kwargs):
@@ -36,28 +38,85 @@ class SearchAPIView(APIView):
         filter_type = request.query_params.get('filter', 'all')
         
         if query:
+            # Check if the query is wrapped in quotes for exact phrase matching
+            exact_match = False
+            if query.startswith('"') and query.endswith('"'):
+                exact_match = True
+                query = query[1:-1]  # Remove the quotes
+            
             # Perform search queries across all models
             documentos = Documento.objects.none()
             personas_no_esclavizadas = PersonaNoEsclavizada.objects.none()
             personas_esclavizadas = PersonaEsclavizada.objects.none()
             corporaciones = Corporacion.objects.none()
+            personas_lugar_rel = PersonaLugarRel.objects.none()
+            
+            # Helper function to create Q objects based on exact match or contains
+            def create_q(field):
+                return Q(**{f"{field}__exact": query}) if exact_match else Q(**{f"{field}__icontains": query})
             
             # Apply filters based on filter type
             if filter_type == 'all' or filter_type == 'documentos':
-                documentos = Documento.objects.filter(Q(titulo__icontains=query) | Q(descripcion__icontains=query))
+                documentos = Documento.objects.filter(
+                    create_q('titulo') | 
+                    create_q('descripcion') |
+                    create_q('documento_idno') |
+                    create_q('fondo') |
+                    create_q('subfondo') |
+                    create_q('serie') |
+                    create_q('subserie') |
+                    create_q('unidad_documental_compuesta') |
+                    create_q('sigla_documento') |
+                    create_q('notas')
+                )
             if filter_type == 'all' or filter_type == 'personas_no_esclavizadas':
-                personas_no_esclavizadas = PersonaNoEsclavizada.objects.filter(Q(nombre_normalizado__icontains=query) | Q(notas__icontains=query))
+                personas_no_esclavizadas = PersonaNoEsclavizada.objects.filter(
+                    create_q('nombre_normalizado') | 
+                    create_q('nombres') |
+                    create_q('apellidos') |
+                    create_q('persona_idno') |
+                    create_q('entidad_asociada') |
+                    create_q('honorifico') |
+                    create_q('notas')
+                )
             if filter_type == 'all' or filter_type == 'personas_esclavizadas':
-                personas_esclavizadas = PersonaEsclavizada.objects.filter(Q(nombre_normalizado__icontains=query) | Q(notas__icontains=query))
+                personas_esclavizadas = PersonaEsclavizada.objects.filter(
+                    create_q('nombre_normalizado') | 
+                    create_q('nombres') |
+                    create_q('apellidos') |
+                    create_q('persona_idno') |
+                    create_q('hispanizacion__hispanizacion') |
+                    create_q('etnonimos__etonimo') |
+                    create_q('procedencia__nombre_lugar') |
+                    create_q('procedencia_adicional') |
+                    create_q('marcas_corporales') |
+                    create_q('conducta') |
+                    create_q('salud') |
+                    create_q('notas')
+                )
             if filter_type == 'all' or filter_type == 'corporaciones':
-                corporaciones = Corporacion.objects.filter(Q(nombre_institucion__icontains=query) | Q(personas_asociadas__nombre_normalizado__icontains=query))
+                corporaciones = Corporacion.objects.filter(
+                    create_q('nombre_institucion') | 
+                    create_q('tipo_institucion__tipo') |
+                    create_q('personas_asociadas__nombre_normalizado') |
+                    create_q('notas')
+                )
+            if filter_type == 'all' or filter_type == 'personas_lugar_rel':
+                personas_lugar_rel = PersonaLugarRel.objects.filter(
+                    create_q('personas__nombre_normalizado') | 
+                    create_q('lugar__nombre_lugar') |
+                    create_q('situacion_lugar__situacion') |
+                    create_q('documento__titulo') |
+                    create_q('notas')
+                )
             
             # Serialize the results without pagination first
             serialized_data = (
                 DocumentoSerializer(documentos, many=True).data +
                 PersonaNoEsclavizadaSerializer(personas_no_esclavizadas, many=True).data +
                 PersonaEsclavizadaSerializer(personas_esclavizadas, many=True).data +
-                CorporacionSerializer(corporaciones, many=True).data
+                CorporacionSerializer(corporaciones, many=True).data +
+                PersonaLugarRelSerializer(personas_lugar_rel, many=True).data
             )
             
             # Combine all serialized results into a single list
