d24a9c2b59a4470007a3e31535a8b11a50b855ff
jairomelo <jairoantoniomelo@gmail.com>
2024-10-29 16:17:52 -0700

    secure ElasticSearch connection

--- mdb/settings.py
+++ mdb/settings.py
@@ -263,7 +263,9 @@ REST_FRAMEWORK = {
 
 ELASTICSEARCH_DSL = {
     'default': {
-        'hosts': 'http://localhost:9200',
+        'hosts': 'https://' + os.getenv('ELASTICSEARCH_HOST') + ':' + os.getenv('ELASTICSEARCH_PORT') if os.getenv('ELASTICSEARCH_PORT') else '',
+        'http_auth': (os.getenv('ELASTICSEARCH_USER'), os.getenv('ELASTICSEARCH_PASSWORD')),
+        'verify_certs': False, # Set to True when deploying to production
     },
 }
 
