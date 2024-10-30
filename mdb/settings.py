82d024d20019f131ade9dbb7f3778a1cc94ef89f
jairomelo <jairoantoniomelo@gmail.com>
2024-10-29 20:43:11 -0700

    verify certs on env

--- mdb/settings.py
+++ mdb/settings.py
@@ -265,7 +265,7 @@ ELASTICSEARCH_DSL = {
     'default': {
         'hosts': 'https://' + os.getenv('ELASTICSEARCH_HOST') + ':' + os.getenv('ELASTICSEARCH_PORT') if os.getenv('ELASTICSEARCH_PORT') else '',
         'http_auth': (os.getenv('ELASTICSEARCH_USER'), os.getenv('ELASTICSEARCH_PASSWORD')),
-        'verify_certs': True, # Set to True when deploying to production
+        'verify_certs': os.getenv('ELASTICSEARCH_VERIFY_CERTS') if os.getenv('ELASTICSEARCH_VERIFY_CERTS') else True,
     },
 }
 
