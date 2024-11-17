3c2d1b17b7a9f2fb4fa2b59685b9eaecbafac4ac
jairomelo <jairoantoniomelo@gmail.com>
2024-11-16 20:25:02 -0800

    increase page_size pagination

--- api/v1beta/views.py
+++ api/v1beta/views.py
@@ -46,7 +46,7 @@ class APIPerm(BasePermission):
         return request.user and (request.user.is_staff or request.user.groups.filter(name="colectores").exists())
 
 class CustomPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
 
